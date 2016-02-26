0456540bf7b417ca346827e70eaddd25aa782a93
Jesse Hughes <Jesse Hughes>
2016-02-26 10:15:37 -0600

    I commented out the composer update, it was failing likely due to the updates being in code anyway.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Jessehuze/se-group8.git /home/vagrant/Code/
-( cd /home/vagrant/Code/laraveltripplanner/ && composer update )
+# ( cd /home/vagrant/Code/laraveltripplanner/ && composer update )
