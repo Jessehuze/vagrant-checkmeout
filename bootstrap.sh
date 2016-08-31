84e6d43b3f2718cc7ad8883db8948434db55530f
jessehuze <jbhf39@mst.edu>
2016-08-30 21:37:21 -0500

    Fixed git clone errors.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/nrhvyc/checkmeout-lumen.git /home/vagrant/Code/
-git clone https://github.com/nate437/checkmeout-frontend.git /home/vagrant/Code
+git clone https://github.com/nrhvyc/checkmeout-lumen.git /home/vagrant/Code/checkmeout-lumen
+git clone https://github.com/nate437/checkmeout-frontend.git /home/vagrant/Code/checkmeout-frontend
 ( cd /home/vagrant/Code/checkmeout-lumen/ && composer install )
