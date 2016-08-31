87a97261b3d695c745578036b58461bccd4e7c0a
jessehuze <jbhf39@mst.edu>
2016-08-30 21:08:13 -0500

    Updated files for checkmeout architecture.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/Jessehuze/se-group8.git /home/vagrant/Code/
-( cd /home/vagrant/Code/laraveltripplanner/ && composer install )
+git clone https://github.com/nrhvyc/checkmeout-lumen.git /home/vagrant/Code/
+git clone https://github.com/nate437/checkmeout-frontend.git /home/vagrant/Code
+( cd /home/vagrant/Code/checkmeout-lumen/ && composer install )
