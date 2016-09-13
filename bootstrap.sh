442e5b693625d4d5a0a1ee6b93c9711ffe437afb
jessehuze <jbhf39@mst.edu>
2016-09-13 13:56:43 -0500

    Almost done - working on webpack: command not found error

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Backend - Lumen build
 git clone https://github.com/nrhvyc/checkmeout-lumen.git /home/vagrant/Code/checkmeout-lumen
+
+# Frontend
 git clone https://github.com/nate437/checkmeout-frontend.git /home/vagrant/Code/checkmeout-frontend
 ( cd /home/vagrant/Code/checkmeout-lumen/ && composer install )
+
+( cd /home/vagrant/Code/checkmeout-frontend/ && npm install --no-bin-links )
+# ( cd /home/vagrant/Code/checkmeout-frontend/ && npm install typescript -g )
+# ( cd /home/vagrant/Code/checkmeout-frontend/ && npm install ts-loader webpack@^1.12.6 node-sass css-loader sass-loader style-loader react@^15.3.1 react-dom react-router --no-bin-links )
+
+# ( cd /home/vagrant/Code/checkmeout-frontend/ && webpack )
