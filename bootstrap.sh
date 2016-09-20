041602ef2de2bdbd2a523df86c5729ec6efade22
Jesse Hughes <Jesse Hughes>
2016-09-20 16:19:11 -0500

    Fixed front-end issues.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,6 @@ git clone https://github.com/nrhvyc/checkmeout-lumen.git /home/vagrant/Code/chec
 git clone https://github.com/nate437/checkmeout-frontend.git /home/vagrant/Code/checkmeout-frontend
 ( cd /home/vagrant/Code/checkmeout-lumen/ && composer install )
 
-( cd /home/vagrant/Code/checkmeout-frontend/ && npm install --no-bin-links )
-# ( cd /home/vagrant/Code/checkmeout-frontend/ && npm install typescript -g )
-# ( cd /home/vagrant/Code/checkmeout-frontend/ && npm install ts-loader webpack@^1.12.6 node-sass css-loader sass-loader style-loader react@^15.3.1 react-dom react-router --no-bin-links )
-
-# ( cd /home/vagrant/Code/checkmeout-frontend/ && webpack )
+( cd /home/vagrant/Code/checkmeout-frontend/ && sudo npm install --no-bin-links )
+( cd /home/vagrant/Code/checkmeout-frontend/ && sudo npm install webpack -g )
+( cd /home/vagrant/Code/checkmeout-frontend/ && webpack )
